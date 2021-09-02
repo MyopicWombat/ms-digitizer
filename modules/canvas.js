c0098b5222bc3b60627d4b3b86bfe58ea7d5faa4
Jeremy Hudson <Jeremy.Hudson@Milliken.com>
2021-09-02 17:18:01 -0400

    working on refactoring canvas drawing

--- modules/canvas.js
+++ modules/canvas.js
@@ -1,3 +1,5 @@
+import { context } from "../gui";
+
 export class MSImage {
   #canvas
   #context;
@@ -21,14 +23,23 @@ export class MSImage {
       color
     }
   }
-  updateRule(name, location, color){
+  updateRule(name, location) {
     this.#rulers[name] = {
       location,
       color
     }
   }
+  deleteRule(name) {
+    delete this.#rulers[name];
+  }
   drawRules() {
+    for (let i = 0; i < this.#rules.length; i++) {
 
+    }
+  }
+  redrawImage() {
+    this.#context.clearRect(0, 0, this.#canvas.width, this.#canvas.height);
+    this.#context.putImageData(this.#savedImage, 0, 0);
   }
 }
 
@@ -46,20 +57,20 @@ export const drawMarkers = (context, markers) => {
   }
 
 
-const drawVerticalRule = (context, x, color = 0) => {
-  let height = context.canvas.height;
-  drawLine(context, x, 0, x, height, color);
-}
+  const drawVerticalRule = (context, x, color = 0) => {
+    let height = context.canvas.height;
+    drawLine(context, x, 0, x, height, color);
+  }
 
-const drawHorizontalRule = (context, y, color = 0) => {
-  let width = context.canvas.width;
-  drawLine(context, 0, y, width, y, color);
-}
+  const drawHorizontalRule = (context, y, color = 0) => {
+    let width = context.canvas.width;
+    drawLine(context, 0, y, width, y, color);
+  }
 
-const drawLine = (context, x1, y1, x2, y2, color = 0) => {
-  context.beginPath();
-  context.moveTo(x1, y1);
-  context.lineTo(x2, y2);
-  context.strokeStyle = color;
-  context.stroke();
-}
+  const drawLine = (context, x1, y1, x2, y2, color = 0) => {
+    context.beginPath();
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.strokeStyle = color;
+    context.stroke();
+  }
